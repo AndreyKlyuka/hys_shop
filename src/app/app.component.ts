55453477b1f5b24e171d7de4bfad1cdf7de19ddf
AndrejKlukaa <andrej.kljuka@gmail.com>
2022-12-01 19:52:54 +0200

    ls_14 create new project, config some base setting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+interface IData {
+  id: number;
+  name: string;
+  price: number;
+}
+
+function generateData(length: number): IData[] {
+  return [];
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'hys_angular-app';
+  public data: IData[] = [];
 }
