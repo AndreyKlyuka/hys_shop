776c2dfc0ac0f3965f7913a6616b084a3a4a25b5
AndrejKlukaa <andrej.kljuka@gmail.com>
2022-12-05 18:25:19 +0200

    ls_14 create page structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-import { IData } from 'src/models/interfaces/data.interface';
-
-function generateData(length: number): IData[] {
-  return [];
-}
+import { IData } from '../models/interfaces/data.interface';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +7,20 @@ function generateData(length: number): IData[] {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public data: IData[] = [];
+  public products: IData[] = this.generateData(8);
+
+  private generateData(length: number): IData[] {
+    const data: IData[] = [];
+
+    for (let i = 0; i < length; i++) {
+      const product: IData = {
+        id: i,
+        name: Math.random().toString(36).slice(2, 8),
+        price: +(Math.random() * 100).toFixed() * 10,
+      };
+
+      data.push(product);
+    }
+    return data;
+  }
 }
