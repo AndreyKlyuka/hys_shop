0e5e6703f862c6221057dbc2ff574e08fb54ba2c
AndrejKlukaa <andrej.kljuka@gmail.com>
2022-12-07 17:04:08 +0200

    ls_14 refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
-import { IData } from '../models/interfaces/data.interface';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../models/interfaces/product.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  public products: IData[] = this.generateData(8);
+export class AppComponent implements OnInit {
+  public products: IProduct[] = this.generateData(8);
 
-  private generateData(length: number): IData[] {
-    const data: IData[] = [];
+  constructor() {}
+
+  ngOnInit() {
+    this.products = this.generateData(8);
+  }
+
+  private generateData(length: number): IProduct[] {
+    const data: IProduct[] = [];
 
     for (let i = 0; i < length; i++) {
-      const product: IData = {
+      const product: IProduct = {
         id: i,
         name: Math.random().toString(36).slice(2, 8),
         price: +(Math.random() * 100).toFixed() * 10,
--- src/models/interfaces/product.interface.ts
+++ src/models/interfaces/product.interface.ts
@@ -1,4 +1,4 @@
-export interface IData {
+export interface IProduct {
   id: number;
   name: string;
   price: number;
