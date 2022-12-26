344a37a7c6b73ab9e68d3cc2cb63794d67f6bbe8
AndrejKlukaa <andrej.kljuka@gmail.com>
2022-12-26 18:07:35 +0200

    ls_17 refactor shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { ProductsComponent } from '@pages/products/products.component';
-import { ProductsService } from '@pages/products/products.service';
-import { ProductCardComponent } from '@shared/components/product-card/product-card.component';
-import { HomeComponent } from '@pages/home/home.component';
-import { ProductComponent } from '@pages/product/product.component';
-import { CartComponent } from '@pages/cart/cart.component';
-
-import { NotFoundComponent } from '@pages/not-found/not-found.component';
 import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductsComponent,
-    ProductCardComponent,
-    HomeComponent,
-    ProductComponent,
-    CartComponent,
-    NotFoundComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, SharedModule],
-  providers: [ProductsService],
+  providers: [],
   bootstrap: [AppComponent],
   exports: [],
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { UahCurrencyPipe } from '@shared/pipes/uah-currency.pipe';
 import { PriceColorDirective } from '@shared/directives/price-color.directive';
 import { FooterComponent } from '@core/footer/footer.component';
@@ -8,6 +9,13 @@ import { CartTooltipComponent } from '@shared/components/cart-tooltip/cart-toolt
 import { RouterLink } from '@angular/router';
 import { AdminSidebarComponent } from './components/admin-sidebar/admin-sidebar.component';
 import { AdminTableComponent } from './components/admin-table/admin-table.component';
+import { NotFoundComponent } from '@pages/not-found/not-found.component';
+import { CartComponent } from '@pages/cart/cart.component';
+import { ProductCardComponent } from '@shared/components/product-card/product-card.component';
+import { ProductComponent } from '@pages/product/product.component';
+import { HomeComponent } from '@pages/home/home.component';
+import { ProductsService } from '@pages/products/products.service';
+import { ProductsComponent } from '@pages/products/products.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +26,14 @@ import { AdminTableComponent } from './components/admin-table/admin-table.compon
     CartTooltipComponent,
     AdminSidebarComponent,
     AdminTableComponent,
+    NotFoundComponent,
+    CartComponent,
+    ProductCardComponent,
+    ProductComponent,
+    HomeComponent,
+    ProductsComponent,
   ],
+  providers: [ProductsService],
   imports: [CommonModule, RouterLink],
   exports: [
     UahCurrencyPipe,
@@ -28,6 +43,12 @@ import { AdminTableComponent } from './components/admin-table/admin-table.compon
     CartTooltipComponent,
     AdminSidebarComponent,
     AdminTableComponent,
+    NotFoundComponent,
+    CartComponent,
+    ProductCardComponent,
+    ProductComponent,
+    HomeComponent,
+    ProductsComponent,
   ],
 })
 export class SharedModule {}
